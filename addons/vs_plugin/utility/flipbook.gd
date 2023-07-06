f74039f2451d7e9121154100eb4c4364e8f2b50e
Kextex <kextex3465@gmail.com>
2023-07-06 18:55:28 +0800

    update flipbook shader

--- addons/vs_plugin/utility/flipbook.gd
+++ addons/vs_plugin/utility/flipbook.gd
@@ -20,52 +20,57 @@ func _get_return_icon_type() -> PortType:
 
 
 func _get_input_port_count() -> int:
-	return 1
+	return 2
 
 
 func _get_input_port_name(port):
 	match port:
 		0:
-			return "uv";
+			return "uv in"
+		1:
+			return "Speed"
 
 
 func _get_input_port_type(port):
 	match port:
 		0:
-			return PORT_TYPE_VECTOR_2D;
-
+			return PORT_TYPE_VECTOR_2D
+		1:
+			return PORT_TYPE_SCALAR
 
 
 func _get_output_port_count() -> int:
 	return 1
 
-
 func _get_output_port_name(port) -> String:
-	return "color"
+	return "uv out"
 
 
 func _get_output_port_type(port) -> PortType:
-	return PORT_TYPE_VECTOR_4D
+	return PORT_TYPE_VECTOR_2D
+
 
 func _get_global_code(mode) -> String:
 	var code = preload("flipbook.gdshader").code
 	code = code.replace("shader_type spatial;\n", "")
 	return code
 	
+
 func _get_code(input_vars: Array[String], output_vars: Array[String], mode: Shader.Mode, type: VisualShader.Type) -> String:
 	# Default values
-	var uv = "UV"
-	var time = "TIME"
+	var uv: String = "UV"
+	var rate: String = "2.5"
 	
 	if input_vars[0]:
 		uv = input_vars[0]
-	# if input_vars[1]:
-	# 	time = input_vars[1]
+	if input_vars[1]:
+		rate = input_vars[1]
 		
-	var params =  [uv, time, output_vars[0]]
-	return "fLiPBook(%s, %s, %s);" % params
+	var params =  [uv, rate, output_vars[0]]
+	return "fLiPBook(%s, %s, TIME, %s);" % params
 	
-#func _init():
+
+func _init():
 	# Default values for Editor
-#	if not get_input_port_default_value(1):
-#		set_input_port_default_value(1, 1.7)
+	if not get_input_port_default_value(1):
+		set_input_port_default_value(1, 2.5)
