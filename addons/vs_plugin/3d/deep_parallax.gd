6be603ce590d9da1be563fbd98f0548c7c5073e9
Kextex <kextex3465@gmail.com>
2023-08-16 15:07:31 +0800

    update_parallax

--- addons/vs_plugin/3d/deep_parallax.gd
+++ addons/vs_plugin/3d/deep_parallax.gd
@@ -18,7 +18,7 @@ func _get_return_icon_type():
 	return VisualShaderNode.PORT_TYPE_SCALAR
 
 func _get_input_port_count():
-	return 5
+	return 6
 
 func _get_input_port_name(port):
 	match port:
@@ -27,21 +27,14 @@ func _get_input_port_name(port):
 		1:
 			return "texture"
 		2:
-			return "depth_scale"
+			return "heightmap_scale"
 		3:
-			return "base layers"
+			return "min layers"
 		4:
-			return "detail layers"
-		# 5:
-		# 	return "(vertex)"
-		# 6:
-		# 	return "(normal)"
-		# 7:
-		# 	return "(tangent)"
-		# 8:
-		# 	return "(binormal)"
-		# 9:
-		# 	return "(depth_flip)"
+			return "max layers"
+		5:
+			return "depth_flip"
+
 
 func _get_input_port_type(port):
 	match port:
@@ -52,19 +45,12 @@ func _get_input_port_type(port):
 		2:
 			return VisualShaderNode.PORT_TYPE_SCALAR
 		3:
-			return VisualShaderNode.PORT_TYPE_SCALAR
+			return VisualShaderNode.PORT_TYPE_SCALAR_INT
 		4:
-			return VisualShaderNode.PORT_TYPE_SCALAR
-		# 5:
-		# 	return VisualShaderNode.PORT_TYPE_VECTOR_3D
-		# 6:
-		# 	return VisualShaderNode.PORT_TYPE_VECTOR_3D
-		# 7:
-		# 	return VisualShaderNode.PORT_TYPE_VECTOR_3D
-		# 8:
-		# 	return VisualShaderNode.PORT_TYPE_VECTOR_3D
-		# 9:
-		# 	return VisualShaderNode.PORT_TYPE_VECTOR_3D
+			return VisualShaderNode.PORT_TYPE_SCALAR_INT
+		5:
+			return VisualShaderNode.PORT_TYPE_VECTOR_2D
+
 
 func _get_output_port_count():
 	return 1
@@ -95,37 +81,31 @@ func _get_code(input_vars, output_vars, mode, type):
 	var uv = "UV"
 	var texture = "texture_parallax_default"
 #	var depth_scale = "0.05"
-	var depth_scale = "depth_scalar"
-	var base_layers = "128.0"
-	var detail_layers = "64.0"
+	var heightmap_scale = "heightmap_scale"
+	var min_layers = "8"
+	var max_layers = "32"
 	var vertex = "VERTEX"
 	var normal = "NORMAL"
 	var tangent = "TANGENT"
 	var binormal = "BINORMAL"
-	var depth_flip = "vec3(1.0, 1.0, 0.0)"
+	var eyeoffset = "EYE_OFFSET"
+	var depth_flip = "_depth_flip"
 
 	if input_vars[0]:
 		uv = input_vars[0]
 	if input_vars[1]:
 		texture = input_vars[1]
 	if input_vars[2]:
-		depth_scale = input_vars[2]	
+		heightmap_scale = input_vars[2]	
 	if input_vars[3]:
-		base_layers = input_vars[3]
+		min_layers = input_vars[3]
 	if input_vars[4]:
-		detail_layers = input_vars[4]	
-	# if input_vars[5]:
-	# 	vertex = input_vars[5]
-	# if input_vars[6]:
-	# 	normal = input_vars[6]
-	# if input_vars[7]:
-	# 	tangent = input_vars[7]
-	# if input_vars[8]:
-	# 	binormal = input_vars[8]
-	# if input_vars[9]:
-	# 	depth_flip = input_vars[9]
+		max_layers = input_vars[4]	
+	if input_vars[5]:
+		depth_flip = input_vars[5]
 
-	var params =  [uv, texture, depth_scale, base_layers, detail_layers, vertex, normal, tangent, binormal, depth_flip, output_vars[0]]
+
+	var params =  [uv, texture, heightmap_scale, min_layers, max_layers, vertex, normal, tangent, binormal, eyeoffset, output_vars[0]]
 	return "deep_parallax(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);" % params
 
 func _init(): 
@@ -135,10 +115,10 @@ func _init():
 #		set_input_port_default_value(2, 0.05)
 	# min_layers
 	if not get_input_port_default_value(3):
-		set_input_port_default_value(3, 128.0)
+		set_input_port_default_value(3, 8)
 	# max_layers
 	if not get_input_port_default_value(4):
-		set_input_port_default_value(4, 64.0)
+		set_input_port_default_value(4, 32)
 	# depth_flip
-#	if not get_input_port_default_value(9):
-	set_input_port_default_value(9, Vector3(1.0, 1.0, 0.0))
+	if not get_input_port_default_value(5):
+		set_input_port_default_value(5, Vector2(1.0, 1.0))
